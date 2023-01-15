48396d00564ab9aa5226463bd6eb6f657965b0eb
Michael <mikahado@gmail.com>
2023-01-15 17:43:43 -0500

    adds Application Controller routes

--- server-backend/app/controllers/application_controller.rb
+++ server-backend/app/controllers/application_controller.rb
@@ -1,6 +1,47 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  # routes
+
+  get '/' do
+    "home"
+  end
+
+  get '/workspaces' do
+    workspaces = Workspace.all
+    workspaces.to_json
+  end
+
+  get '/workspaces/:id' do
+    workspace = Workspace.find(params[:id])
+    workspace.to_json
+  end
+
+  post '/workspaces' do
+    workspace = Workspace.create(
+    name: params[:name],
+    address: params[:address]
+    )
+    workspace.to_json
+  end
+
+  delete '/workspaces/:id' do
+    workspace = Workspace.find(params[:id])
+    workspace.destroy
+    workspace.to_json
+  end
+
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  post '/reviews/:id' do
+    review = Review.create(
+      rating: params[:rating],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
 end
